9d793ec92e23582a39ec947eaf1d988fe30dee01
Coman Games <alfaa.gen@gmail.com>
2017-02-03 20:52:11 +0200

    I done the changes

--- MethodOveriding/Program.cs
+++ MethodOveriding/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,98 @@ namespace MethodOveriding
 {
     class Program
     {
-        static void Main(string[] args)
+       public static  void Main(string[] args)
         {
+            
+            Animal animal = new Animal("Bobik",100);
+            AskAnimalToSay(animal);
+
+            Animal dog = new Dog("Rex",10);
+
+            AskAnimalToSay(dog);
+
+            Animal labrador = new Labrador("Botsman",20);
+            AskAnimalToSay(labrador);
+            Console.ReadKey();
+            
+
         }
+
+        private static void AskAnimalToSay(Animal animal)
+        {
+            Console.WriteLine(animal.ToString());
+            Console.WriteLine($"Animal {animal.Name} give sound");
+            animal.Say();
+            Console.WriteLine(animal.ToString());
+            Console.WriteLine();
+        }
+
+        public static void Run()
+        {
+
+        }
+    }
+
+    public class Labrador :Dog 
+    {
+        public Labrador(string name, int food): base("",0)
+        {
+            this.Food = food*2;
+            this.Name = "King "+ name;
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Hau! Hau!");
+        }
+
+    }
+    public class Dog:Animal
+    {
+        public Dog(string name, int food) : base(name, food)
+        {
+            Console.WriteLine("We call constructor with 2 parameters in dog");
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine("We call method say in Dog");
+            base.Say();
+            Console.WriteLine("Hau!");
+
+        }
+    }
+    public class Animal
+    {
+        public int Food;
+        public float Speed;
+        public string Name;
+
+        public Animal(string name, int Food, float Speed)
+        {
+           this.Food = Food;
+           this.Speed = Speed;
+            this.Name = name;
+        }
+
+        public Animal(string name, int food) : this(name, food, 0)
+        {
+            Console.WriteLine("We call constructor with 2 parameters in Animal");
+        }
+
+        public Animal(string name) : this(name,0 ) { }
+
+        public Animal(): this("No Name") { }
+
+        public override string ToString()
+        {
+            return $"{nameof(Food)}: {Food}, {nameof(Speed)}: {Speed}, {nameof(Name)}: {Name}";
+        }
+
+        public virtual void Say()
+        {
+            Food--;
+            Console.WriteLine("We call method Say in Animal");
+        }
+
     }
 }
